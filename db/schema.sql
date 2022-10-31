604fc811b54386aa97b367c01456763b2f71ea41
A.Klein <allenkleined@gmail.com>
2022-10-30 19:30:09 -0700

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-drop database if exists employee_db;
-create database employee_db;
-use employee_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL,
@@ -8,18 +8,22 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE rolee (
+CREATE TABLE role (
   id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary Decimal NOT NULL,
-  FOREIGN KEY (id)
+  department_id INT,
+  PRIMARY KEY (id)
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 CREATE TABLE employee(
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    Rolee_id INT NOT NULL,
-    
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
     manager_id INT
 );
