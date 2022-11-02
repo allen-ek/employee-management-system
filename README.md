4959bebf91d5a214e70dbd1662c3034de95c45b7
A.Klein <allenkleined@gmail.com>
2022-11-01 20:28:20 -0700

    finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # employee-management-system
+Allen Klein
+
+- [Link to Video](https://watch.screencastify.com/v/cI31vFmKuKEt2mOoePGt)
+- [Link to Github Repository](https://github.com/allen-ek/employee-management-system)
+
+
+## Why?
+I wanted to create a command line application that would be able to create an employee database to display employee information.
+
+## What I learned
+I learned how to use Node.js with MySQL to create and query a database to showcase relavent information.
+## Technologies Used
+MYSQL2 package
+Node.js
+MySQL
+Github
+
+## Code Snippet
+```html
+function displayEmployees(){
+    let query=
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary, CONCAT(manager.first_name, ',', manager.last_name) AS manager
+  FROM employee 
+  LEFT OUTER JOIN role
+	ON employee.role_id = role.id
+  LEFT OUTER JOIN department 
+  ON department.id = role.department_id
+  LEFT OUTER JOIN employee manager
+	ON manager.id = employee.manager_id`;
+    db.query(query, function (err, data){ 
+        console.table(data);
+    });
+    Options();
+};
+```
+The code snippet above was the code in order to handle an input from the command line to show case from the employee database the employees informations
+
--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const db = mysql.createConnection(
   );
   
   Options();
-//functions
+
+
+  //functions
 function Options(){
     inquirer.prompt({
             type : "list",
@@ -25,21 +27,25 @@ function Options(){
             choices:["view all departments", "view all roles", 
             "view all employees", "add a department", "add a role", "add an employee","update employee role"],
             name: "options"
-        })
-        .then(function({options}){
+        }).then(function({options}){
             switch (options){
                 case "view all departments":
                     displayDepartments();
                 break;
                 case"view all roles":
-                displayRoles();
+                    displayRoles();
                 break;
                 case"view all employees":
-                displayRoles();
+                    displayEmployees();
+                break;
+                case"add a department":
+                    addDepartment();
+                break;
+                case"add a a role":
+                    addRole();
                 break;
             }
-
-    });
+        });
 }
 function displayRoles(){
     let query=`SELECT * FROM role`;
@@ -59,6 +65,82 @@ function displayDepartments(){
 
 
 function displayEmployees(){
-    let query=``;
+    let query=
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary, CONCAT(manager.first_name, ',', manager.last_name) AS manager
+  FROM employee 
+  LEFT OUTER JOIN role
+	ON employee.role_id = role.id
+  LEFT OUTER JOIN department 
+  ON department.id = role.department_id
+  LEFT OUTER JOIN employee manager
+	ON manager.id = employee.manager_id`;
+    db.query(query, function (err, data){ 
+        console.table(data);
+    });
     Options();
+};
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"depName",
+            message:"What is the department name?"
+        }
+    ]).then(function(answers){
+        let query='INSERT INTO department (department_name) VALUES (?)';
+        db.query(query,[answers.depName],function(err,data){
+            if (err) throw err;
+            console.table("added department");
+            Options();
+        })
+    });
+};
+function addRole(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"roleName",
+            message:"What is the role name?"
+        },
+        
+        {
+            type: "input",
+            name:"salary",
+            message:"What is the salary ?"
+        },
+        {
+            type: "input",
+            name:"roleName",
+            message:"Which department does the role belong ?"
+        }
+    ])
+};
+
+    //wip
+    function addEmployee(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"fName",
+            message:"What is Employees first name?"
+        },
+        {
+            type: "input",
+            name:"lName",
+            message:"What is Employees last name?"
+        },
+        {
+            type: "input",
+            name:"fName",
+            message:"What is Employees role?"
+        },
+]).then(function(answers){
+
+   // let query=`INSERT * FROM department`;
+   // db.query(query, function (err, data){ 
+   //     console.table(err);
+   //     })
+     Options();
+    })
 };
